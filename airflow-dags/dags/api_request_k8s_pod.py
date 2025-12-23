a43c96f10bc60753e2bc50175db44a50c9d2eb28
AnhQuanNguyen1 <181691243+AnhQuanNguyen1@users.noreply.github.com>
2025-12-23 11:15:30 +0700

    update

---
+++ airflow-dags/dags/api_request_k8s_pod.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+
+
+default_args = {
+    "description": "Orchestrate weather API ingestion by running a K8s pod",
+    "start_date": datetime(2025, 12, 22),
+    "catchup": False,
+}
+
+with DAG(
+    dag_id="weather-api-orchestrator",
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=5),
+    catchup=False,
+    max_active_runs=1,
+    tags=["k8s", "api-request"],
+) as dag:
+
+    ingest_data_task = KubernetesPodOperator(
+        task_id="ingest_data_task",
+        name="api-request-run",
+        namespace="api-request", 
+        image="naq113469/api-request:latest",
+        image_pull_policy="Always",
+        service_account_name="api-request", 
+
+        # Nếu image PRIVATE thì bỏ comment và dùng đúng secret name (vd: regcred)
+        # image_pull_secrets=[k8s.V1LocalObjectReference("regcred")],
+
+        in_cluster=True,
+        get_logs=True,
+        is_delete_operator_pod=True,
+        do_xcom_push=False,
+
+        cmds=["/bin/sh", "-c"],
+        arguments=["python -c 'from insert_records_clickhouse import main; main()'"],
+
+        # env_vars={
+        #     "ENV": "prod",
+        #     # "CLICKHOUSE_HOST": "...",
+        #     # "CLICKHOUSE_USER": "...",
+        #     # "CLICKHOUSE_PASSWORD": "...",
+        # },
+
+        labels={"app": "api-request", "dag": "weather-api-orchestrator"},
+    )
